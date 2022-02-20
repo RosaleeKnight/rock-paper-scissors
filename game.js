6dbc687bac8b7db15a4933b9e2af60e20a531682
RosaleeKnight <rtknight92@outlook.com>
2022-02-19 21:51:37 -0600

    Edit Index.html and game.js

---
+++ game.js
@@ -0,0 +1,5 @@
+const computerPlay= ["rock", "paper", "scissors"];
+        
+        function random_item(computerPlay) {
+            return computerPlay[Math.floor(Math.random()*computerPlay.length)];
+            }
