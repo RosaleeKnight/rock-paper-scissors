ce2bdb88b497bce0df411cd59f5f57f6c0644ccd
RosaleeKnight <rtknight92@outlook.com>
2022-02-20 08:37:25 -0600

    Edit game.js

--- game.js
+++ game.js
@@ -7,7 +7,21 @@ const game = () => {
         const rockBtn = document.querySelector('.rock')
         const paperBtn = document.querySelector('.paper')
         const scissorsBtn = document.querySelector('.scissor')
-        const playerOptions = [rockBtn, paperBtn, scissorsBtn]
-        const computerOptions = ['rock', 'paper', 'scissors']
+        const playerOptions = [rockBtn,paperBtn,scissorsBtn]
+        const computerOptions = ['rock','paper','scissors']
+
+        playerOptions.forEach(option => {
+            option.addEventListener('click',function(){
+                const movesLeft = document.querySelector('.movesLeft');
+                moves++;
+                movesLeft.innerText = `Moves Left: ${5-moves}`;
+                const choiceNumber = Math.floor(Math.random()*3);
+                const computerChoice = computerOptions[choiceNumber];
+                winner(this.innerText,computerChoice)
+                if(moves == 5){
+                    gameOver(playerOptions,movesLeft);
+                }
+            })
+        })
     }
 }
