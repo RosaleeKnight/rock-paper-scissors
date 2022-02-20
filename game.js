3aba9a7bb1c15050812b2cdbf027f64f0d402269
RosaleeKnight <rtknight92@outlook.com>
2022-02-20 08:22:40 -0600

    Edit index.html and game.js

--- game.js
+++ game.js
@@ -1,5 +1,13 @@
-const computerPlay= ["rock", "paper", "scissors"];
-        
-        function random_item(computerPlay) {
-            return computerPlay[Math.floor(Math.random()*computerPlay.length)];
-            }
+const game = () => {
+    let playerScore = 0
+    let computerScore = 0
+    let moves = 0
+
+    const playGame = () => {
+        const rockBtn = document.querySelector('.rock')
+        const paperBtn = document.querySelector('.paper')
+        const scissorsBtn = document.querySelector('.scissor')
+        const playerOptions = [rockBtn, paperBtn, scissorsBtn]
+        const computerOptions = ['rock', 'paper', 'scissors']
+    }
+}
